00e84efcc0f0eda1565da34f2ebae76e23ce20d3
usernameAnda <aurasagita2122gmail.com>
2023-12-11 11:12:35 +0700

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DokterController;
+use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\PasienController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
+
+Auth::routes();
+Route::get('/logout', [LoginController::class, 'logout']);
+Route::middleware(['auth'])->group(function(){
+  Route::get('/dashboard', [DashboardController::class, 'index']);
+  Route::resource('/dokter', DokterController::class)->parameter('dokter', 'id');
+  Route::resource('/pasien', PasienController::class)->parameter('pasien', 'id');
+  Route::resource('/jadwal', JadwalController::class)->parameter('jadwal', 'id');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
