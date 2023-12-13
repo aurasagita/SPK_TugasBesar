4261b29b993ec3c73f45df31eeccaea268cf30f5
Naegoya <nabilahsyafa69@gmail.com>
2023-12-13 10:03:00 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-  //  return view('welcome');
-//});
+Route::get('/', function () {
+  return view('welcome');
+});
 
 Auth::routes();
 Route::get('/logout', [LoginController::class, 'logout']);
